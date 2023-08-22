9b1adaf1f57ca4f888c9f6119a235ca7e5840cdb
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2023-08-22 12:26:16 -0300

    Update checkout-script.js

--- js/checkout-script.js
+++ js/checkout-script.js
@@ -133,7 +133,7 @@ const TrafiCheckout = {
             "quantity": quantity,
             "unit_price": variant.price,
             "unit_price_total": TrafiCheckout._.formatValue(variant.price * quantity),
-            "image_url": variant.images[0].src,
+            "image_url": variant.images[0]?.src ?? product.images[0]?.src,
             "vendor": product.vendor_product.vendor_id,
             "shop": product.vendor_product.store_id,
             "discounts_total": rest.discount,
@@ -147,68 +147,6 @@ const TrafiCheckout = {
         },
         create: async (cart) => TrafiCheckout._.updateCart(cart),
         update: async (cart) => TrafiCheckout._.updateCart(cart),
-        transform: {
-            shopify: (cart) => {
-
-                // Yes. Why.
-                // A conundrum. That's why.
-                // So here's the deal, on one side, cart.js does not return the right compare at price. On the other hand, the cart drop
-                // in Liquid no longer has the token available. Since we need both things to build the proper payload, got to handle both scenarios.
-                const items = cart.items.map(item => ({
-                    "tags": [],
-                    "type": item.product_type,
-                    "product_id": item.product_id,
-                    "variant_id": item.variant_id,
-                    "sku": item.sku,
-                    "compare_at_price": parseFloat(
-                        (item.original_price / 100).toFixed(3)
-                    ),
-                    "compare_at_price_total": parseFloat(
-                        (item.original_price * item.quantity / 100).toFixed(3)
-                    ),
-                    "name": item.product_title,
-                    "description": item.variant_title || 'description',
-                    "quantity": item.quantity,
-                    "unit_price": parseFloat(
-                        (item.final_price / 100).toFixed(3)
-                    ),
-                    "unit_price_total": parseFloat(
-                        (item.final_line_price / 100).toFixed(3)
-                    ),
-                    "image_url": item.image,
-                    "vendor": headless.vendor,
-                    "shop": headless.store,
-                    "discounts_total": parseFloat(
-                        (item.total_discount / 100).toFixed(3)
-                    ) || 0,
-                    "discount_label": 'BUNDLE APPLIED',
-                    "subscription": item.properties?.payload ?
-                        (item.variant_id == item.properties.variantId ?
-                            item.properties.payload.subscription
-                            : null)
-                        : null
-                }));
-
-
-                const totals = {
-                    products: parseFloat(
-                        (cart.total_price / 100).toFixed(3)
-                    ),
-                    discount: parseFloat(
-                        (cart.total_discount / 100).toFixed(3)
-                    ) || 0,
-                    total: parseFloat(
-                        (cart.total_price / 100).toFixed(3)
-                    ),
-                    shipping: 0,
-                    taxes: 0
-                };
-
-                return { items, totals }
-            }
-            ,
-            big_commerce: (cart) => cart
-        },
     },
     checkout: {
         redirect: async (cartId) => {
