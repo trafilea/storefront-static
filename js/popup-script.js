e9e96b48f4d6dc18c180d787598152b3e28f35cf
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2023-12-22 12:33:34 -0300

    feat: popup

--- js/popup-script.js
+++ js/popup-script.js
@@ -1,20 +1,22 @@
 let defaultConfig = {
   name: "TrafiPopup",
-  triggered_by: "",
+  triggered_by: ".openPopup",
   title: "CONGRATULATIONS!",
-  subtitle: "You have unlocked a special offer!",
+  subtitle: "You have unlocked a <b>special offer!</b>",
   label: "Hurry! This FREE gift item is going FAST.",
   image:
-    "https://cdn.shopify.com/s/files/1/0509/6331/6893/files/Free-Gift-Image.png?v=1628190919",
+    "https://cdn.shopify.com/s/files/1/0912/0596/files/konjac_1_607a6a43-e9d7-4087-aded-14fe74842e33.jpg?v=1692224035",
+  price: "FREE",
+  compare_at_price: "$9.95",
   buttons: {
     accept: {
-      text: "Add to Cart",
+      text: "Yes, I want this!",
       class: "",
       onClick: () => {},
       trigger: "",
     },
     decline: {
-      text: "No Thanks",
+      text: "No thanks",
       class: "",
       onClick: () => {},
       trigger: "",
@@ -39,64 +41,6 @@ const TrafiPopup = (overrideConfig = defaultConfig) => {
     popup.id = config.name;
     popup.innerHTML = `
                             <style>
-                            .new__loader,
-                            .new__loader:before,
-                            .new__loader:after {
-                            border-radius: 50%;
-                            width: 2.5em;
-                            height: 2.5em;
-                            -webkit-animation-fill-mode: both;
-                            animation-fill-mode: both;
-                            -webkit-animation: load7 1.8s infinite ease-in-out;
-                            animation: load7 1.8s infinite ease-in-out;
-                            }
-                            .new__loader {
-                            color: #000000;
-                            font-size: 10px;
-                            margin: 10px auto 50px;
-                            position: relative;
-                            text-indent: -9999em;
-                            -webkit-transform: translateZ(0);
-                            -ms-transform: translateZ(0);
-                            transform: translateZ(0);
-                            -webkit-animation-delay: -0.16s;
-                            animation-delay: -0.16s;
-                            }
-                            .new__loader:before,
-                            .new__loader:after {
-                            content: '';
-                            position: absolute;
-                            top: 0;
-                            }
-                            .new__loader:before {
-                            left: -3.5em;
-                            -webkit-animation-delay: -0.32s;
-                            animation-delay: -0.32s;
-                            }
-                            .new__loader:after {
-                            left: 3.5em;
-                            }
-                            @-webkit-keyframes load7 {
-                            0%,
-                            80%,
-                            100% {
-                            box-shadow: 0 2.5em 0 -1.3em;
-                            }
-                            40% {
-                            box-shadow: 0 2.5em 0 0;
-                            }
-                            }
-                            @keyframes load7 {
-                            0%,
-                            80%,
-                            100% {
-                            box-shadow: 0 2.5em 0 -1.3em;
-                            }
-                            40% {
-                            box-shadow: 0 2.5em 0 0;
-                            }
-                            }
-
                             .loading-overlay {
                             position: fixed;
                             top: 0;
@@ -137,6 +81,7 @@ const TrafiPopup = (overrideConfig = defaultConfig) => {
                               gap: 16px;
                             }
                             .pp_buttons button {
+                              cursor: pointer;
                               height: 50px;
                               padding: 0px 10px;
                               justify-content: center;
@@ -154,16 +99,24 @@ const TrafiPopup = (overrideConfig = defaultConfig) => {
                               font-weight: 700;
                               line-height: 22px;
                             }
+
+                            .pp_decline {
+                              background: white;
+                              border: 1px solid #FFC700;
+                            }
+
                             .pp_buttons button:hover {
                               background: #FFD800;
                             }
+
                             .pp_title {
                               font-size: 16px;
                               line-height: 24px;
                               text-transform: uppercase;
                               font-weight: bold;
-                              color: rgb(69, 122, 65);
+                              color: #457a41;
                             }
+
                             .pp_subtitle {
                               color: rgb(41, 41, 41);
                               font-size: 16px;
@@ -172,16 +125,54 @@ const TrafiPopup = (overrideConfig = defaultConfig) => {
                               text-align: center;
                               margin-bottom: 10px;
                             }
+
                             .pp_label {
                               font-size: 14px;
                               line-height: 22px;
                               font-weight: 500;
-                              color: rgb(69, 122, 65);
+                              color: #457a41;
                             }
+
                             .pp_image {
-                              width: 100%;
-                              height: auto;
                               margin-bottom: 10px;
+                              border: 2px solid rgb(69, 122, 65);
+                              border-radius: 20px;
+                              width: 80%;
+                              height: auto;
+                            }
+
+                            .css-141nkgt {
+                              position: relative;
+                              bottom: 250px;
+                              margin-bottom: -40px;
+                              left: 40%;
+                            }
+
+                            .css-nnhxy {
+                              background-color: rgb(186, 83, 80);
+                              width: 70px;
+                              height: 70px;
+                              border-radius: 50%;
+                              display: flex;
+                              flex-direction: column;
+                              -webkit-box-align: center;
+                              align-items: center;
+                              -webkit-box-pack: center;
+                              justify-content: center;
+                            }
+                            .css-vgmnak {
+                              font-size: 14px;
+                              line-height: 18px;
+                              color: white;
+                              text-decoration: line-through;
+                              margin-right: 5px;
+                            }
+                            .css-101f6se {
+                              font-size: 14px;
+                              line-height: 18px;
+                              color: white;
+                              margin-right: 5px;
+                              font-weight: bold;
                             }
                             </style>
                             <div class="loading-overlay">
@@ -191,6 +182,7 @@ const TrafiPopup = (overrideConfig = defaultConfig) => {
                                     <h3 class="pp_subtitle">${config.subtitle}</h2>
                                     <h6 class="pp_label">${config.label}</h6>
                                     <img class="pp_image" src="${config.image}" alt="Popup Image" />
+                                    <div class="css-141nkgt"><div class="css-nnhxy"><div class="css-vgmnak">${config.compare_at_price}</div><div class="css-101f6se">${config.price}</div></div></div>
                                     <div class="pp_buttons">
                                       <button class="pp_accept ${config.buttons.accept.class}">${config.buttons.accept.text}</button>
                                       <button class="pp_decline ${config.buttons.decline.class}">${config.buttons.decline.text}</button>
